cc4a3586377f763727c954410607c59b1f3cb5b7
Jared Hamby <irjamby@gmail.com>
2013-03-02 13:52:43 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DotaTicker::Application.config.secret_token = 'b76b3160d3cd69534799de054dfa2aed835e98788a90219a98b404239b9a667de4b69cd0798ce2d943bcdcb6a8018350d566ce21739dbc0604c127801caccbde'
